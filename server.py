223dbbdaaa8a2feb28b37a4204aeb290200feb74
Ben Weston <35574350+bweston6@users.noreply.github.com>
2021-05-26 21:16:22 +0100

    Initial commit

---
+++ server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template, Response, url_for
+from os import system as cmd
+from irBlaster import blast
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/numbers')
+def numbers():
+	return render_template('numbers.html')
+
+@app.route('/extra')
+def extra():
+	return render_template('extra.html')
+
+@app.route('/query/<keyName>')
+def keyHandler(keyName):
+	blast(keyName)
+	print("Blasted " + keyName)
+	return "nothing"
+    
+@app.route('/redirect/<keyName>')
+def redirectHandler(keyName):
+	blast(keyName)
+	print("Blasted " + keyName)
+	return index()
+    
+## Start webserver
+if __name__ == '__main__':
+	from waitress import serve
+	serve(app, host='0.0.0.0', port=80)
